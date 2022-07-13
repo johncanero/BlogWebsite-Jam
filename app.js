8727524f86d3ad70e1852ee567519b287455d020
John Canero <johncanero.official@gmail.com>
2022-07-13 11:24:42 +0800

    Update About Description

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const _ = require("lodash");
 // contents
 const homeStartingContent = "Welcome to my Daily Journal! I am John Cañero. 👋 I am an aspiring web developer, programmer, architectural designer and visual artist. I created this journal guided by Angela Yu while studying her Web Development BootCamp Course. This would be a great journey for me to document my life and adventures especially in the world of technology.When I was learning this type of technology, it helped me to start building something on the Internet.🎨 Let's go and build something that is valuable and helpful for the world!";
 
-const aboutContent = "Hey!👋 You can call me Jam, it is my nickname! ";
+const aboutContent = "Hey!👋 You can call me Jam, it is my nickname! Enjoy my daily journal and hopefully, you could also create your own, regardless of physically in a notebook or digitally, taking notes and thoughts about your life. By the way, I am currently in my Senior Year as a BS Architecture Student. 👨‍🎨 Yup! Also at the side, I am studying Web Development and exploring the wonders of technology. You could visit my Github Page and Personal Website I created at the links below.";
+
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 // essential: express
